9476aa745986d942cfba820b02853bf67c5ff5fa
drsimmo <simon.otoole@aao.gov.au>
2019-03-31 00:17:28 +1100

    Testing

--- src/mod_auth_cas.c
+++ src/mod_auth_cas.c
@@ -2466,6 +2466,7 @@ authz_status cas_check_authorization(request_rec *r,
 				ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
 					      "Require cas-attribute "
 					      "'%s' matched", output);
+			free(tt);
 			return AUTHZ_GRANTED;
 		}
 	}
