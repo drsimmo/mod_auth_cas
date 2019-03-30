aeb5c886b2459cde654c35e751cfb7d3dec06fd0
drsimmo <simon.otoole@aao.gov.au>
2019-03-30 14:25:40 +1100

    Added section to cas_check_authorization to evaulate expressions.

--- src/mod_auth_cas.c
+++ src/mod_auth_cas.c
@@ -2419,8 +2419,11 @@ authz_status cas_check_authorization(request_rec *r,
 	const cas_cfg *const c = ap_get_module_config(r->server->module_config, &auth_cas_module);
 	const cas_saml_attr *const attrs = cas_get_attributes(r);
 
-	const char *t, *w, *ww;
+	const char *t, *w, *ww, *expr, *err;
 	unsigned int count_casattr = 0;
+	apr_pool_t *temp_pool;
+	apr_status_t *tpstat;
+	ap_expr_info_t $info;
 
 	if(c->CASDebug)
 		ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
@@ -2431,15 +2434,29 @@ authz_status cas_check_authorization(request_rec *r,
 	t = require_line;
 	while ((w = ap_getword_conf(r->pool, &t)) && w[0]) {
 		count_casattr++;
-		ww = ap_resolve_env(r->pool,w);
-		if (cas_match_attribute(ww, attrs, r) == CAS_ATTR_MATCH) {
+		/* Check to see if there are any expressions that need 
+		 * parsing, especially variables with functions */
+		tpstat = apr_pool_create(&temp_pool,NULL);
+		ww = ap_expr_parse(r->pool,temp_pool,info,w,NULL);
+		if (!ww) {
+			expr = ap_expr_str_exec(r,info,&err);
+		} else {
+			if(c->CASDebug)
+				ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+		      	"Could not parse expression.");
+		}
+		if (err) {
+			if(c->CASDebug)
+				ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, err);
+		}
+		if (cas_match_attribute(expr, attrs, r) == CAS_ATTR_MATCH) {
 			/* If *any* attribute matches, then
 			 * authorization has succeeded and all
 			 * of the others are ignored. */
 			if(c->CASDebug)
 				ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
 					      "Require cas-attribute "
-					      "'%s' matched", ww);
+					      "'%s' matched", expr);
 			return AUTHZ_GRANTED;
 		}
 	}
