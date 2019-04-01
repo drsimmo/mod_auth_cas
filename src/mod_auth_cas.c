04de143658acf28b76c90eddf4d7928bb9861298
drsimmo <simon.otoole@aao.gov.au>
2019-04-01 11:34:46 +1100

    Fixed types

--- src/mod_auth_cas.c
+++ src/mod_auth_cas.c
@@ -2419,7 +2419,8 @@ authz_status cas_check_authorization(request_rec *r,
 	const cas_cfg *const c = ap_get_module_config(r->server->module_config, &auth_cas_module);
 	const cas_saml_attr *const attrs = cas_get_attributes(r);
 
-	const char *t, *w, *output;
+	const char *t;
+	char *w, *output;
 	unsigned int count_casattr = 0;
 
 	if(c->CASDebug)
@@ -2460,7 +2461,7 @@ const char cas_check_expressions(request_rec *r, char *word) {
 		const cas_cfg *const c = ap_get_module_config(r->server->module_config, &auth_cas_module);
 
 		const char *e, *err, *tt, *ww;
-		const char *output = malloc(sizeof(*output));
+		char *output = malloc(sizeof(*output));
 		int index;
 
 		apr_pool_t *temp_pool;
